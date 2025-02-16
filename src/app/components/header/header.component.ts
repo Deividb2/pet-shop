77b5aff3bc28f835c2da6ce1ddd8af234a4ce853
Deividb2 <deividbarreto57@gmail.com>
2025-02-16 17:30:12 -0300

    componente header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core'
+import { matClose, matMenu } from '@ng-icons/material-icons/baseline'
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
+  providers: [provideIcons({ matMenu, matClose })],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  iconName: string = 'matMenu'
+  menu: boolean = false
 
+  hendleMenu() {
+    this.menu = !this.menu
+    this.menu ? this.iconName = 'matClose' : this.iconName = 'matMenu'
+  }
 }
