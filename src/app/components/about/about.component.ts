ad3c79e3dbaf70d46125b76de3798f4b6728498a
Deividb2 <deividbarreto57@gmail.com>
2025-02-19 18:07:33 -0300

    criando componente about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matArrowRightAlt } from '@ng-icons/material-icons/baseline';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
+  providers: [provideIcons({ matArrowRightAlt })],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
