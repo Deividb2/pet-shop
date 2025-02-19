52253f788b47118998f2ad9907ff4970581a54cb
Deividb2 <deividbarreto57@gmail.com>
2025-02-19 17:27:47 -0300

    criando o componente de cards

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matArrowRightAlt } from '@ng-icons/material-icons/baseline';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIcon],
+  providers: [provideIcons({ matArrowRightAlt })],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss'
 })
 export class CardsComponent {
 
+  cards: ReadonlyArray<ICards> = [
+    {
+      imgSrc: "../../../assets/images/little-cat.jpg",
+      title: "Gatos",
+      subtitle: "Lorem ipsum maecenas porttitor at tincidunt elit euismod"
+    },
+    {
+      imgSrc: "../../../assets/images/cat-dog2.jpg",
+      title: "Filhotes",
+      subtitle: "Lorem ipsum maecenas porttitor at tincidunt elit euismod"
+    },
+    {
+      imgSrc: "../../../assets/images/dog1.jpg",
+      title: "Cachorros",
+      subtitle: "Lorem ipsum maecenas porttitor at tincidunt elit euismod"
+    }
+  ]
+
+}
+
+interface ICards {
+  imgSrc: string
+  title: string
+  subtitle: string
 }
