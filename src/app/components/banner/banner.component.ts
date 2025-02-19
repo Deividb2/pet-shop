85a5e668b55f704b262aced142c0dcc086f91494
Deividb2 <deividbarreto57@gmail.com>
2025-02-19 15:26:41 -0300

    criando componentes banner e range

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matArrowRight, matArrowRightAlt } from '@ng-icons/material-icons/baseline';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
+  providers: [provideIcons({ matArrowRightAlt })],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
