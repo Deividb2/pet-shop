3a34ef95e00796677fe4b07152b3f26447505eaa
Deividb2 <deividbarreto57@gmail.com>
2025-05-22 15:27:10 -0300

    criando seção de feedback

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,42 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { matStar } from '@ng-icons/material-icons/baseline';
+import { matStar, matMarkUnreadChatAlt } from '@ng-icons/material-icons/baseline';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
   imports: [CommonModule, NgIcon],
-  providers: [provideIcons({ matStar })],
+  providers: [provideIcons({ matStar, matMarkUnreadChatAlt })],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
 })
 export class FeedbackComponent {
   feedbacks: ReadonlyArray<{ name: string, text: string, lengthStars: number }> = [
     {
-      name: "Lula",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis at esse harum, labore voluptatum.",
+      name: "Julia Oliveira",
+      text: "Maravilhosos! Meu cachorro foi fazer uma tosa higiênica + tomar banho pela primeira vez fora de casa e voltou limpinho e cheiroso!",
       lengthStars: 5
     },
     {
-      name: "Bolsonaro",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis at esse harum, labore voluptatum.",
+      name: "Larissa Alonso",
+      text: "Local ótimo, sempre levo meu cachorro lá. Super atenciosos e ainda buscam em casa. Obrigada a todos pelo carinho",
       lengthStars: 4
     },
     {
-      name: "Chaves",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis at esse harum, labore voluptatum.",
+      name: "Mary Alves",
+      text: "Excelente, cuidar bem do meu Dog , tosa deles é excelente Equipe maravilhosa , indico sempre !",
       lengthStars: 5
     },
     {
-      name: "Nego Ney",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis at esse harum, labore voluptatum.",
+      name: "Valeria Moreira",
+      text: "Como sempre eles se superam no quesito Profissionalismo. Super indico. Eu amei a atenção e o cuidado com meu Luke",
       lengthStars: 4
-    },
-    {
-      name: "The king the power",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis at esse harum, labore voluptatum.",
-      lengthStars: 5
-    },
+    }
   ]
 }
